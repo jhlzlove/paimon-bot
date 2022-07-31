d13921f7049376f7142d2d708577c4c34038caa2
jhlz <jhlzlove@163.com>
2022-07-31 11:31:41 +0800

    refactor: 💡 添加获取成员列表

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 基于QQ官方的 Node SDK 开发，详情查看 [官方文档介绍](https://bot.q.qq.com/wiki/develop/nodesdk/#%E4%BB%8B%E7%BB%8D)。
 
+
+```bash
+src/__test__ ：测试文件
+src：源 js 文件
+config：配置文件
+docs：文档文件
+resources：资源文件
+```
+
--- src/bot.js
+++ src/bot.js
@@ -12,11 +12,17 @@ const ws = createWebsocket(config.bot);
 let guildMap = new Map()
 // 子频道信息
 let channelMap = new Map()
-// TODO 频道成员
+// 频道成员
+let memberMap = new Map()
+// 频道机器人
+let botCount = 0
 
 async function load() {
   // 机器人信息
   let user = await client.meApi.me();
+  console.log(`================ ${user.data.username} 上线成功！！！===============`);
+  console.log(`=================== 正在加载信息，请稍后...... ======================`);
+
 
   // 频道信息
   let guild = await client.meApi.meGuilds();
@@ -30,15 +36,34 @@ async function load() {
     /* 子频道 */
     let channels = await client.channelApi.channels(guildData[i].id)
     let channelData = channels.data
+    console.log(channelData);
     for (let j = 0; j < channelData.length; j++) {
       // 键：channelId    值：channelName
       channelMap.set(channelData[j].id, channelData[j].name)
     }
+
+    // 频道成员列表 第二个参数频道没有那么多人不要了
+    let members = await client.guildApi.guildMembers(guildData[i].id);
+    let memberData = members.data
+    console.log(memberData);
+    for (let k = 0; k < memberData.length; k++) {
+      if (memberData[k].user.bot) {
+        botCount++
+      } else {
+        // 键：user.id   值：member
+        memberMap.set(memberData[k].user.id, memberData[k])
+      }
+      
+    }
+
   }
 
-  console.log(`================ ${user.data.username} 上线成功！！！===============`);
-  console.log(`=================== 正在加载信息，请稍后...... ======================`);
+  
   console.log(`======== 加载了 ${guildMap.size} 个频道，${channelMap.size} 个子频道 =========`);
+  console.log(`======== 加载了 ${memberMap.size} 个成员，${botCount} 个机器人 =========`);
+  console.log(memberMap);
+
+
   console.log(`============================= 加载完毕！ ============================`);
 }
 
@@ -73,6 +98,11 @@ ws.on('GUILD_MEMBERS', data => {
   // }
 });
 ws.on('GUILD_MESSAGES', data => {
+  client.messageApi.postMessage(data.msg.channel_id, {content: "还不滚去学习"}).then((res) => {
+    console.log(res.data);
+  }).catch((err) => {
+    console.log(err);
+  })
   console.log('[GUILD_MESSAGES] 事件接收 :', data);
 });
 ws.on('GUILD_MESSAGE_REACTIONS', data => {
